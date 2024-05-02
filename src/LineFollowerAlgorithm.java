c40ec8ab3746dae21f100a4dfdac5f700700e29f
Monnapse <masonshuber@gmail.com>
2024-05-02 13:32:46 -0600

    save

--- src/LineFollowerAlgorithm.java
+++ src/LineFollowerAlgorithm.java
@@ -2,7 +2,11 @@ import java.sql.Array;
 import java.util.ArrayList;
 
 public class LineFollowerAlgorithm {
-    int MidPoint = 40;
+    int MidPoint;
+
+    public LineFollowerAlgorithm (int midPoint) {
+        this.MidPoint = midPoint;
+    }
     private boolean IsLine(int sensorNumber) {
         //System.out.println(sensorNumber);
         return sensorNumber < MidPoint;
@@ -22,20 +26,47 @@ public class LineFollowerAlgorithm {
         int sensorsOverLine = detectedSensorsOverLine.size();
 
         if (sensorsOverLine > 0) {
-            int midpoint = 0;
-            if (sensorsOverLine > 1) {
-                int sum = 0;
-                for (int i = 0; i < sensorsOverLine; i++) {
-                    int position = (sensorsOverLine - 1) - i;
-                    sum += (sensorsOverLine + 1) - (position);
-                }
-                midpoint = sum/2;
-            } else {
-                detectedSensorsOverLine.get(0);
-            }
-            return (double) midpoint /(sensorCount*2-1);
+            //int mid = sensorCount/2;
+            int point = detectedSensorsOverLine.get(0);//getMidpoint(sensorsOverLine, detectedSensorsOverLine);
+
+            //System.out.printf("%nSENSOR COUNT: %d", sensorCount);
+            int divisor = 100/(sensorCount-1);
+            return (double) (point * divisor)/100;
         }
 
         return -1.0;
+    }
+
+    //private static int getMidpoint(int sensorsOverLine, ArrayList<Integer> detectedSensorsOverLine) {
+    //    int midpoint = 2; // 2 = center
+//
+    //    if (sensorsOverLine > 1) {
+    //        int sum = 0;
+    //        for (int i = 0; i < sensorsOverLine; i++) {
+    //            int sensorPosition = detectedSensorsOverLine.get(i);
+    //            //int position = (sensorsOverLine - 1) - sensorPosition;
+    //            //System.out.printf("%f %d has line under it", (sensorsOverLine + 1) - position);
+    //            sum += sensorPosition;
+    //        }
+    //        midpoint = sum/2;
+    //    } else {
+    //        midpoint = detectedSensorsOverLine.get(0);
+    //    }
+//
+    //    return midpoint;
+    //}
+
+    public double GetLinePositionSplitInversion(double position) {
+        //double newPosition = 0.0;
+        if (position == 0.5) { return 0; }
+        //int normal = 1;
+        //if (position < 0.5) {normal = -1;}
+        //else { position -= 0.5; }
+        return -position*2+1;
+    }
+
+    public double GetAngle(double percentage, double minAngle, double maxAngle, double rate) {
+        double angle = ((minAngle / maxAngle) + Math.pow(percentage, 2))*rate;
+        return Math.min(angle, maxAngle);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,21 @@ import com.pi4j.wiringpi.Gpio;
 import java.io.IOException;
 
 public class Main {
+    // Configurations
+    static final Pin RED_SWITCH = RaspiPin.GPIO_26;
+    static final Pin GREEN_SWITCH = RaspiPin.GPIO_25;
+    static final Pin RED_LED = RaspiPin.GPIO_27;
+    static final Pin GREEN_LED = RaspiPin.GPIO_24;
+    static final double MAX_ANGLE = 25.0;
+    static final double MIN_ANGLE = 1;
+    static final double RATE = 35;
+    static final int MIDPOINT = 50;
+    static boolean CarTurnedOn = false;
+
     public static void main(String[] args) throws IOException, I2CFactory.UnsupportedBusNumberException, InterruptedException {
         //final boolean DEBUGGING = true;
         GpioController controller = GpioFactory.getInstance();
-        LineFollowerAlgorithm LFA = new LineFollowerAlgorithm();
+        LineFollowerAlgorithm LFA = new LineFollowerAlgorithm(MIDPOINT);
 
         // WORK IN PROGRESS FOR LATER
         //ButtonsManager buttonManager = new ButtonsManager(controller);
@@ -19,11 +30,11 @@ public class Main {
 
         // GPIOS
         //Switches
-        GpioPinDigitalInput redSwitch = controller.provisionDigitalInputPin(RaspiPin.GPIO_26);
-        GpioPinDigitalInput greenSwitch = controller.provisionDigitalInputPin(RaspiPin.GPIO_25);
+        GpioPinDigitalInput redSwitch = controller.provisionDigitalInputPin(RED_SWITCH);
+        GpioPinDigitalInput greenSwitch = controller.provisionDigitalInputPin(GREEN_SWITCH);
         // LED's
-        GpioPinDigitalOutput redLed = controller.provisionDigitalOutputPin(RaspiPin.GPIO_27, PinState.LOW);
-        GpioPinDigitalOutput greenLed = controller.provisionDigitalOutputPin(RaspiPin.GPIO_24, PinState.LOW);
+        GpioPinDigitalOutput redLed = controller.provisionDigitalOutputPin(RED_LED, PinState.LOW);
+        GpioPinDigitalOutput greenLed = controller.provisionDigitalOutputPin(GREEN_LED, PinState.LOW);
 
         // Motor's
         Motors motors = new Motors();
@@ -51,11 +62,34 @@ public class Main {
         //    }
         //});
 
+        greenSwitch.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                if (event.getState() == PinState.HIGH) {
+                    CarTurnedOn = !CarTurnedOn;
+                    if (CarTurnedOn) {greenLed.setState(PinState.HIGH);}
+                    else {greenLed.setState(PinState.LOW);}
+                }
+            }
+        });
+
         while (true) {
-            int[] sensorData = new int[5];
-            sensorData = lightSensor.readSensorValues();
-            double linePosition = LFA.GetLinePosition(sensorData);
-            //System.out.printf("%n%d, %d, %d, %d, %d", sensorData[0],sensorData[1],sensorData[2],sensorData[3],sensorData[4]);
+            if (CarTurnedOn) {
+                int[] sensorData;
+                sensorData = lightSensor.readSensorValues();
+                double linePosition = LFA.GetLinePosition(sensorData);
+                //System.out.printf("%n%d, %d, %d, %d, %d", sensorData[0],sensorData[1],sensorData[2],sensorData[3],sensorData[4]);
+                if (linePosition != -1.0) {
+                    double lineSplitInversion = LFA.GetLinePositionSplitInversion(linePosition);
+                    double angle = 0;
+                    if (lineSplitInversion != 0) {
+                        angle = LFA.GetAngle(lineSplitInversion, MIN_ANGLE, MAX_ANGLE, RATE);
+                    }
+                    System.out.printf("%n Line Position: %.2f %n Line Split Inversion: %.2f %n Servo Angle: %.2f", linePosition, lineSplitInversion, angle);
+
+                    servo.setServoAngle((int) angle);
+                }
+            }
 
             Thread.sleep(250);
         }
